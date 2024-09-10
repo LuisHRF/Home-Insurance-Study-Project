a643bb3561fdce0a1f2ac80c02813d841c5a6860
Luis H Rodríguez <160022546+LuisHRF@users.noreply.github.com>
2024-09-10 11:49:25 +0200

    Primera subida con la limpieza

---
+++ functions.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+def defi_years_per_block(data_frame, year_range):
+
+    data_frame_cp = data_frame.copy()
+
+    for (first_row, last_row), year in year_range.items():
+        data_frame_cp.loc[first_row:last_row, 'Year'] = year
+
+    return data_frame_cp
+
