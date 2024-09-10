b218860073225027ecd30d8ea28e680d8b8818af
David Moreno <124578675+davidmoreno-gh@users.noreply.github.com>
2024-09-10 16:05:34 +0200

    Actualizando función datatype

--- functions.py
+++ functions.py
@@ -11,19 +11,38 @@ def defi_years_per_block(data_frame, year_range):
     return data_frame_cp
 
 
-def verify_and_switch_datatypes(data_clean, assigned_types):
+def verify_and_switch_datatypes(data_clean, column_name, assigned_types):
 
+    column_name = data_clean[column_name]
+    assigned_types = {'Province': str,
+                      'Assault': int,
+                      'physical injuries':int,
+                      'small robberies':int,
+                      'robberies_with_force':int,
+                      'robberies_force_vehicles': int, 
+                      'hard_robbvehicles':int, 
+                      'robberies_force_homes':int, 
+                      'robberies_force_establishments':int, 
+                      'robberies_violence_intimidation':int,
+                      'robberies_violence_publicways':int,
+                      'robberies_violence_homes':int, 
+                      'robberies_violence_establishments':int
+                      }
+    
     # Verificar los tipos de datos actuales y convertir si es necesario
-    for column, assigned_types in assigned_types.items():
-        currently_type = data_clean[column].dtype
+    for column_name, assigned_types in assigned_types.items():
+        
+        currently_type = data_clean[column_name].dtype
         
         if currently_type != assigned_types:
             try:
-                data_clean[column] = data_clean[column].astype(assigned_types)
-                print(f"Column '{column}' converted from {currently_type} a {assigned_types}.")
+                data_clean[column_name] = data_clean[column_name].astype(assigned_types)
+                print(f"Column '{column_name}' converted from {currently_type} a {assigned_types}.")
             except Exception as e:
-                print(f"No se pudo convertir la columna '{column}' a {assigned_types}: {e}")
+                print(f"It was no possible to switch the '{column_name}' type into {assigned_types}: {e}")
         else:
-            print(f"Column '{column}' is currently right typed as ({currently_type}).")
+            print(f"Column '{column_name}' is currently right typed as ({currently_type}).")
     
     return data_clean
+
+    "robberies_violence_intimidation","robberies_violence_publicways","robberies_violence_homes","robberies_violence_establishments"
