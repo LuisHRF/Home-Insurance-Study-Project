83f9655d0aeb6949e00030286e197b910e6557a6
Luis H Rodríguez <160022546+LuisHRF@users.noreply.github.com>
2024-09-11 14:31:19 +0200

    Subida del requirements.txt

--- app.py
+++ app.py
@@ -13,41 +13,46 @@ def interpolate_color(value, min_value, max_value, color1, color2):
     b = int(color1[2] * (1 - ratio) + color2[2] * ratio)
     return f"#{r:02x}{g:02x}{b:02x}"
 
-data_pivot_table = pd.read_csv('data_pivot_table.csv')
+def main():
 
-data_melted = pd.melt(data_pivot_table, id_vars=['Province', 'Latitude', 'Longitude'], 
-                      var_name='Year', value_name='Total_crimes')
+    data_pivot_table = pd.read_csv('data_pivot_table.csv')
 
-data_melted['Year'] = data_melted['Year'].astype(int)
+    data_melted = pd.melt(data_pivot_table, id_vars=['Province', 'Latitude', 'Longitude'], 
+                        var_name='Year', value_name='Total_crimes')
 
-
-years = data_melted['Year'].unique().tolist() 
-selected_year = st.selectbox('Select Year', sorted(years))
-
-data_filtered = data_melted[data_melted['Year'] == selected_year]
+    data_melted['Year'] = data_melted['Year'].astype(int)
 
 
-spain_map = folium.Map(location=[40.0, -3.7], zoom_start=6)
+    years = data_melted['Year'].unique().tolist() 
+    selected_year = st.selectbox('Select Year', sorted(years))
 
-max_crimes = data_filtered['Total_crimes'].max()
-min_crimes = data_filtered['Total_crimes'].min()
+    data_filtered = data_melted[data_melted['Year'] == selected_year]
 
-data_filtered['Log_crimes'] = np.log1p(data_filtered['Total_crimes'])
 
-for index, row in data_filtered.iterrows():
-    radius = (row['Log_crimes'] - data_filtered['Log_crimes'].min()) / (data_filtered['Log_crimes'].max() - data_filtered['Log_crimes'].min()) * 15 + 5
-    color = interpolate_color(row['Total_crimes'], min_crimes, max_crimes, (0, 0, 255), (255, 0, 0))
+    spain_map = folium.Map(location=[40.0, -3.7], zoom_start=6)
 
-    folium.CircleMarker(
-        location=[row['Latitude'], row['Longitude']],
-        radius=radius,  
-        color=color,  
-        fill=True,
-        fill_color=color,  
-        fill_opacity=0.6,
-        popup=f"{row['Province']}: {row['Total_crimes']} crimes",
-        tooltip=row['Province']
-    ).add_to(spain_map)
+    max_crimes = data_filtered['Total_crimes'].max()
+    min_crimes = data_filtered['Total_crimes'].min()
 
-st.title("Crimes per province and year map")
-folium_static(spain_map)
+    data_filtered['Log_crimes'] = np.log1p(data_filtered['Total_crimes'])
+
+    for index, row in data_filtered.iterrows():
+        radius = (row['Log_crimes'] - data_filtered['Log_crimes'].min()) / (data_filtered['Log_crimes'].max() - data_filtered['Log_crimes'].min()) ** 1.5  * 15 + 5
+        color = interpolate_color(row['Total_crimes'], min_crimes, max_crimes, (0, 0, 255), (255, 0, 0))
+
+        folium.CircleMarker(
+            location=[row['Latitude'], row['Longitude']],
+            radius=radius,  
+            color=color,  
+            fill=True,
+            fill_color=color,  
+            fill_opacity=0.6,
+            popup=f"{row['Province']}: {row['Total_crimes']} crimes",
+            tooltip=row['Province']
+        ).add_to(spain_map)
+
+    st.title("Crimes per province and year map")
+    folium_static(spain_map)
+
+if __name__ == "__main__":
+    main()
